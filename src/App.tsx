4150c5f16dff83f82de208276682903d44ea988c
mswtester <mswtester10417@gmail.com>
2024-03-15 18:54:12 +0900

    update2

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,28 @@ function App() {
   }, [once])
   useEffect(() => setOnce(true), [])
 
+  const onFullscreenButtonClick = async () => {
+    try {
+      // 전체 화면 모드로 전환
+      if (document.documentElement.requestFullscreen) {
+        await document.documentElement.requestFullscreen();
+      } else if ((document.documentElement as any).mozRequestFullScreen) { // Firefox
+        await (document.documentElement as any).mozRequestFullScreen();
+      } else if ((document.documentElement as any).webkitRequestFullscreen) { // Chrome, Safari and Opera
+        await (document.documentElement as any).webkitRequestFullscreen();
+      } else if ((document.documentElement as any).msRequestFullscreen) { // IE/Edge
+        await (document.documentElement as any).msRequestFullscreen();
+      }
+  
+      // 화면 방향을 가로로 고정
+      if (screen.orientation && (screen.orientation as any).lock) {
+        await (screen.orientation as any).lock('landscape')
+      }
+    } catch (error) {
+      console.error(`Error trying to force fullscreen and lock orientation: ${error}`);
+    }
+  };
+
   return (
     <>
       {state === "main" && (
@@ -23,11 +45,7 @@ function App() {
           <h1 className='font-semibold text-3xl flex-1 w-full flex flex-row items-center justify-center'>Mini World</h1>
           <div className="flex-1 w-full flex flex-row items-center justify-center">
             <button className='font-semibold rounded-full border-1 border border-gray-300 text-xl px-8 py-4' onClick={() => {
-              // request full screen
-              const elem = document.documentElement
-              if (elem.requestFullscreen) {
-                elem.requestFullscreen()
-              }
+              onFullscreenButtonClick()
               setState("game")
             }}>Start</button>
           </div>
